c46bd6c51dda500b7f76f0e69a2ac3442387830d
MinMinDing <30560868+MinMinDing@users.noreply.github.com>
2019-03-05 05:32:48 +0800

    计算圆的面积

--- Test/src/test/test.java
+++ Test/src/test/test.java
@@ -1,9 +1,19 @@
 package test;
+/**
+ * 个计算圆面积的Jav
+ */
+import java.util.Scanner;
 
 public class test {
 
 	public static void main(String[] args) {
-		System.out.println("a");
+		double a , c;
+		double b = 3.14;
+		Scanner scanner = new Scanner(System.in);
+		System.out.println("请输入圆半径");
+		a = scanner.nextDouble();
+		c = a*a*b;
+		System.out.println("圆的面积为："+c);
 
 	}
 
